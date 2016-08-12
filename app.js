2f36cc8cdf53d87c58b496b3d09dee165bae717a
Acacia Mack <acacialmack@gmail.com>
2016-08-12 16:28:58 +0100

    able to see data in console. Search working in console

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 angular.module('github', [])
-.controller('dataController', ['$scope','$http', function($scope,$http) {
-
-$scope.username = "amack459";
-$http.get("https://api.github.com/users/"+$scope.username)
-        .success(function(data) {
-            $scope.userData = data;
-            console.log(userData.name);
-        });
+  .controller('dataController', ['$scope','$http', function($scope,$http) {
 
 
+    // var $scope = this;
+    // $scope.username = $scope.login;
+
+
+    //push search into data.items
+    $http.get("https://api.github.com/users?since=135" +'?client_id='+$scope.GITHUB_API_KEY+'&client_secret='+$scope.GITHUB_API_SECRET)
+    .success(function (data) {
+      for(var i = 0; i < data.length; i++){
+        //console.log(data[i].login);  
+        //console.log(data[i].avatar_url); 
+        $scope.userData = data[i];
+        console.log($scope.userData.avatar_url);
+        //$scope.login = data[i].login;username
+        //$scope.avatar = data[i].avatar_url;avatar
+      } 
+    });
+
+    $scope.search = function (){
+      $http.get("https://api.github.com/users/"+$scope.userData.login+'?client_id='+$scope.GITHUB_API_KEY+'&client_secret='+$scope.GITHUB_API_SECRET)
+                .success(function (data) {
+                  $scope.data = data;
+                // $scope.userData.push($scope.username);
+                    console.log(data);
+                });
+  };
 }]);
+
+
+
+// function search(){$http.get("https://api.github.com/users/"+$scope.username)
+//   .success(function(data) {
+//     $scope.userData = data;
+//     console.log($scope.userData.name);
+//   });
+// };
+
+
+
+                  // $http.get("https://api.github.com/search/users?q={query}{&page,per_page,sort,order}")
+                  // .success(function(data){
+                  //   $scope.searchData = data.items;
+                  //   querySearch();
+                    //array of results, which is empty. need to push users into array to access
+                    // console.log($scope.searchData);
+                    // console.log(data);
+
+         // var querySearch = function(data){
+         //    $http.get("https://api.github.com/users/{user}")
+         //    .success(function(data){
+         //      $scope.avatar = data;
+         //      console.log(data)
+         //    });
+         //  };
+
--- dataController.js
+++ dataController.js
@@ -8,14 +8,33 @@ dataController.$inject = ['$http', '$scope'];
 
     vm.test = true;
 
-      vm.username = "amack459";
-      $http.get("https://api.github.com/users/"+vm.username)
-      .success(function(data) {
-        vm.userData = data;
-        console.log(data); 
-      });
 
+
+
+      // vm.username = "amack459";
+      // $http.get("https://api.github.com/users/")
+      // .success(function(data) {
+      //   vm.userData = data;
+      //   console.log(data); 
+      // });
+
+   $scope.search = function (){
+        $http.get("https://api.github.com/users?since=130")
+        .success(function(data){
+          $scope.searchData = data;
+          querySearch();
+          console.log(data);
+        });
+
+      function querySearch(){
+        $http.get($scope.searchData)
+          .success(function(data){
+            $scope.avatar = data;
+            console.log(data)
+          });
+      };
      
   }
+}
 
 
