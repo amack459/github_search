9b22f8dbfd2e36d09f9bd303cd50a1b6b77ff00e
Acacia Mack <acacialmack@gmail.com>
2016-08-15 16:46:13 +0100

    comments and a tag

--- app.js
+++ app.js
@@ -1,69 +1,41 @@
 angular.module('github', [])
   .controller('dataController', ['$scope','$http', function($scope,$http) {
 
+    //KEY and SECRET increase rate limit to maximax amount of possible calls to the API
     GITHUB_API_KEY = "7ed3cc510b1575cb829e";
     GITHUB_API_SECRET = "df75f975812b0e8a74d8976746615c21f0966acc";
      
 
-  // var $scope = this;
-  // $scope.username = $scope.login;
-
-
   //push search into data.items
-  $http.get("https://api.github.com/users?since=135" +'?client_id='+GITHUB_API_KEY+'&client_secret='+GITHUB_API_SECRET)
+  $http.get("https://api.github.com/users" +'?client_id='+GITHUB_API_KEY+'&client_secret='+GITHUB_API_SECRET)
   .success(function (data) {
-    for(var i = 0; i < data.length; i++){
-      //console.log(data[i].login);  
-            //console.log(data[i].avatar_url); 
+    for(var i = 0; i < data.length; i++){ 
             $scope.userData = data[i];
+            //list of first 46 users; can be seen in console
             console.log($scope.userData);
-            //$scope.login = data[i].login;username
-            //$scope.avatar = data[i].avatar_url;avatar
     } 
   });
 
-
-
   $scope.search = function (){
-    
-    var vm = this;
-  $http.get($scope.userData.url)
+   //use the previous information in order to be able to extract information for each user once the search function is called
+    $http.get($scope.userData.url+'?client_id='+GITHUB_API_KEY+'&client_secret='+GITHUB_API_SECRET)
     .success(function (data) {
       $scope.user = data;
         // $scope.userData.push($scope.username);
          console.log(data);
+         repos()
+
       });
   };
 
-  $scope.profile = function(){
-    console.log("click");
+  var repos = function(){
+    //user userData in order to get information about a specific user's repos
+    $http.get($scope.userData.repos_url+'?client_id='+GITHUB_API_KEY+'&client_secret='+GITHUB_API_SECRET) 
+    .success(function (data) {
+      $scope.repos = data;
+      console.log(data);
+    })
   }
 }]);
 
-
-
-    // function search(){$http.get("https://api.github.com/users/"+$scope.username)
-    //   .success(function(data) {
-    //     $scope.userData = data;
-    //     console.log($scope.userData.name);
-    //   });
-    // };
-
-
-
-                      // $http.get("https://api.github.com/search/users?q={query}{&page,per_page,sort,order}")
-                      // .success(function(data){
-                      //   $scope.searchData = data.items;
-                      //   querySearch();
-                        //array of results, which is empty. need to push users into array to access
-                        // console.log($scope.searchData);
-                        // console.log(data);
-
-             // var querySearch = function(data){
-             //    $http.get("https://api.github.com/users/{user}")
-             //    .success(function(data){
-             //      $scope.avatar = data;
-             //      console.log(data)
-             //    });
-             //  };
 
